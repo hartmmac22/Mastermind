86144b64bc89efe06dd6f084106f46f766febd8a
hartmmac22 <59925490+hartmmac22@users.noreply.github.com>
2022-01-20 09:32:45 -0500

    Update MasterMindTester.java

--- MasterMindTester.java
+++ MasterMindTester.java
@@ -1,7 +1,8 @@
 public class MasterMindTester {
 	public static void main(String[] args) {
 		Mastermind m1 = new Mastermind();
-		m1.builBoard();
+		m1.buildBoard();
+		m1.guessBoard();
 		m1.printBoard();
 	}
 }
