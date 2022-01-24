53e84d44681f032f1e0eaf1d6a7fd8c870545418
hartmmac22 <59925490+hartmmac22@users.noreply.github.com>
2022-01-24 09:06:14 -0500

    Update Mastermind.java

--- Mastermind.java
+++ Mastermind.java
@@ -1,10 +1,65 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Mastermind {
 	
 	ArrayList<ArrayList<String>> board = new ArrayList<ArrayList<String>>();
+	Scanner keyboard = new Scanner(System.in);
+	
+	
+	public void guessBoard() {
+		//secret code = ABCD
+		System.out.print("Guess Digit 1: ");
+		String digit1 = keyboard.nextLine();
+		System.out.print("\nGuess Digit 2: ");
+		String digit2 = keyboard.nextLine();
+		System.out.print("\nGuess Digit 3: ");
+		String digit3 = keyboard.nextLine();
+		System.out.print("\nGuess Digit 4: ");
+		String digit4 = keyboard.nextLine();
+		board.get(0).set(0, digit1);
+		board.get(0).set(1, digit2);
+		board.get(0).set(2, digit3);
+		board.get(0).set(3, digit4);
+		
+		String a = "2";
+		String b = "1";
+		//checks right letter wrong place guesses
+		if( board.get(0).get(0).equals("A") || board.get(0).get(0).equals("B") || board.get(0).get(0).equals("C") || board.get(0).get(0).equals("D")) {
+			board.get(1).set(2, ("\n\t\t " + b)); 
+		}
+		if( board.get(0).get(1).equals("A") || board.get(0).get(1).equals("B") || board.get(0).get(1).equals("C") || board.get(0).get(1).equals("D")) {
+			board.get(1).set(3, b); 
+		}
+		if( board.get(0).get(2).equals("A") || board.get(0).get(2).equals("B") || board.get(0).get(2).equals("C") || board.get(0).get(2).equals("D")) {
+			board.get(1).set(1, b); 
+		}
+		if( board.get(0).get(3).equals("A") || board.get(0).get(3).equals("B") || board.get(0).get(3).equals("C") || board.get(0).get(3).equals("D")) {
+			board.get(1).set(0, b); 
+		}
+		//checks correct guesses
+		if( board.get(0).get(0).equals("A")) {
+			board.get(1).set(2, ("\n\t\t " + a)); 
+		}
+	
+		if( board.get(0).get(1).equals("B")) {
+			board.get(1).set(3, a); 
+		}
+	
+		if( board.get(0).get(2).equals("C")) {
+			board.get(1).set(1, a); 
+		}
+	
+		if( board.get(0).get(3).equals("D")) {
+			board.get(1).set(0, a); 
+		}
+		
+		
+		
+	}
 	
 	public void buildBoard() {
+		
 		board.add(new ArrayList<String>());
 		for (int i = 0; i < 4; i++) {
 			board.get(0).add(" ");
@@ -17,7 +72,7 @@ public class Mastermind {
 				board.get(1).add("\n\t\t 0");
 			}
 		}
-		
+	
 		board.add(new ArrayList<String>());
 		for (int i = 0; i < 4; i++) {
 			board.get(2).add(" ");
@@ -30,25 +85,16 @@ public class Mastermind {
 				board.get(3).add("\n\t\t 0");
 			}
 		}
-	}
-	
-	
-	public void guessBoard() {
 		
-		board.get(0).set(0, "A");
-		board.get(0).set(1, "B");
-		board.get(0).set(2, "C");
-		board.get(0).set(3, "D");
+		
 	}
 	
 	
 	public void printBoard() {
-		System.out.println("Guesses         Scores");
+		System.out.println("\nGuesses         Scores");
 		System.out.print(board.get(0));
 		System.out.println("\t" + board.get(1));
 		System.out.print(board.get(2));
 		System.out.println("\t" + board.get(3));
 	}
 }
-
-
